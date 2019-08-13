3d49d1935b255431a5dad0235b424ebd3b31af7b
Rodrigo Rocha <rrocha5@slb.com>
2019-08-13 10:18:30 -0300

    Created Method to generate all combinations

--- IndexCalculator.Repository/Implementation/IndexCalculatorRepository.cs
+++ IndexCalculator.Repository/Implementation/IndexCalculatorRepository.cs
@@ -7,9 +7,12 @@ namespace IndexCalculator.Repository.Implementation
     public class IndexCalculatorRepository : IIndexCalculatorRepository
     {
         private readonly IDictionary<int, int> dict;
+        private List<int> allSubscriptions;
+
         public IndexCalculatorRepository()
         {
             dict = new Dictionary<int, int>();
+            allSubscriptions = new List<int>();
             PopulateDictionary();
         }
         //TODO: maybe change this method return to ValidationResult
@@ -24,10 +27,45 @@ namespace IndexCalculator.Repository.Implementation
 
         private void PopulateDictionary()
         {
-            dict.Add(13579, 1);
-            dict.Add(13597, 2);
-            dict.Add(97513, 3);
-            dict.Add(97531, 4);
+            var str = "13579";
+            var chr = str.ToCharArray();
+            GetPer(chr);
+            allSubscriptions.Sort();
+            for(var i = 0; i < allSubscriptions.Count; i++)
+            {
+                dict.Add(allSubscriptions[i], i + 1);
+            }
+        }
+
+        private static void Swap(ref char a, ref char b)
+        {
+            if (a == b) return;
+
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        private void GetPer(char[] list)
+        {
+            int x = list.Length - 1;
+            GetPer(list, 0, x);
+        }
+
+        private void GetPer(char[] list, int k, int m)
+        {
+            if (k == m)
+            {
+                var subscription = new string(list);
+                allSubscriptions.Add(int.Parse(subscription));
+            }
+            else
+                for (int i = k; i <= m; i++)
+                {
+                    Swap(ref list[k], ref list[i]);
+                    GetPer(list, k + 1, m);
+                    Swap(ref list[k], ref list[i]);
+                }
         }
     }
 }
